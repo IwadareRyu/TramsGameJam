02c20c0b7d29d6f2d947344e1d8871a5fdd34582
DRGO <abokado0901@gmail.com>
2022-08-22 04:57:08 +0900

    プレイヤーを動かした

--- Assets/Iwadare/PlayerController.cs
+++ Assets/Iwadare/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] GameObject _Attack;
+    [SerializeField] float _speed = 5f;
+    
+    Rigidbody2D _rb;
+    float _h;
+    float _v;
+    bool _coolTime;
+    float minas;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _h = Input.GetAxis("Horizontal");
+        _v = Input.GetAxis("Vertical");
+        FlipX(_h);
+        if(Input.GetButtonDown("Fire1") && !_coolTime)
+        {
+            _Attack.gameObject.SetActive(true);
+            _coolTime = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(_h * _speed, _v * _speed);
+    }
+    void FlipX(float Horizontal)
+    {
+        if(Horizontal > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),transform.localScale.y,transform.localScale.z);
+            minas = 1;
+        }
+        else if(Horizontal < 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
+            minas = -1;
+        }
+
     }
 }
