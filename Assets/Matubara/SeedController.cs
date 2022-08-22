b902fbc35a5f72f1df0c62c3ba1eb9e59b6261c5
MatubaraTooru <kurakura.ingen@gmail.com>
2022-08-22 14:55:19 +0900

    種修正

--- Assets/Matubara/SeedController.cs
+++ Assets/Matubara/SeedController.cs
@@ -25,7 +25,7 @@ public class SeedController : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
